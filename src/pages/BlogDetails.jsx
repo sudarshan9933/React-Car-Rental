6276eda97bcc31b474f637961d5f8f8802db47eb
sudarshan9933 <sudarshantripathi00@gmail.com>
2024-08-06 10:07:17 +0530

    new s

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -58,9 +58,7 @@ const BlogDetails = () => {
                     <h6 className=" fw-bold">David Visa</h6>
                     <p className="section__description mb-0">14 July, 2022</p>
                     <p className="section__description">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Eos nobis totam eius laborum molestias itaque minima
-                      distinctio, quae velit tempore!
+                      Very Good Services And Charges Are Customer Friendly 
                     </p>
 
                     <span className="replay d-flex align-items-center gap-1">
